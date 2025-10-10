8da4c9ee79f2422574d718e3949597ecf3cd2e31
Vipooshan Vipulananthan <77396714+Vipooshan-1998@users.noreply.github.com>
2025-10-10 20:56:57 +0530

    Update transnet_model.py

--- models/transnet_model.py
+++ models/transnet_model.py
@@ -672,10 +672,5 @@ class SpaceTempGoG_detr_dad(nn.Module):
         probs_mc = self.softmax(logits_mc)
         check_nan(probs_mc, "probs_mc", print_tensor=True)
     
-        print("logits_mc.shape:", logits_mc.shape,
-              "min/max logits:", logits_mc.min().item(), logits_mc.max().item())
-        print("probs_mc.shape:", probs_mc.shape,
-              "min/max probs:", probs_mc.min().item(), probs_mc.max().item())
-    
         return logits_mc, probs_mc
 
