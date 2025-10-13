ec001a1c15450a5163cce8060ae554e691b2abe9
Vipooshan Vipulananthan <77396714+Vipooshan-1998@users.noreply.github.com>
2025-10-13 20:31:56 +0530

    Update cross_validate_dota.py

--- cross_validate_dota.py
+++ cross_validate_dota.py
@@ -177,7 +177,7 @@ def test_model(epoch, model, test_dataloader, fold):
 def train(train_dataloader, test_dataloader, fold):
 
     # Define network
-    model = SpaceTempGoG_detr_dota(input_dim=opt.input_dim, embedding_dim=opt.embedding_dim,
+    model = Transnet(input_dim=opt.input_dim, embedding_dim=opt.embedding_dim,
                                   img_feat_dim=opt.img_feat_dim, num_classes=opt.num_classes).to(device)
     print(model)
 
