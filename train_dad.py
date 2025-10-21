66cbed100bdf0cec6ebc87947afebf8724774bba
Vipooshan Vipulananthan <77396714+Vipooshan-1998@users.noreply.github.com>
2025-10-21 22:25:08 +0530

    Update train_dad.py

--- train_dad.py
+++ train_dad.py
@@ -203,7 +203,7 @@ def main():
 	test_dataloader = DataLoader(test_dataset, batch_size=opt.test_video_batch_size, shuffle=False, num_workers=8)
 
 	# Define network
-	model = Trans_LSTM_Sans_Obj_Temp_Graph_Trans_Obj(input_dim=opt.input_dim, embedding_dim=opt.embedding_dim, img_feat_dim=opt.img_feat_dim, num_classes=opt.num_classes).to(device)
+	model = TransGated(input_dim=opt.input_dim, embedding_dim=opt.embedding_dim, img_feat_dim=opt.img_feat_dim, num_classes=opt.num_classes).to(device)
 	print(model)
 	
 	model.train()
@@ -327,6 +327,7 @@ def main():
 	
 if __name__ == "__main__":
 	main()
+
 
 
 
