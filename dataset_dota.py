472e88b52a64f7c64efe73958f407091eb79ebe1
Vipooshan Vipulananthan <77396714+Vipooshan-1998@users.noreply.github.com>
2025-10-25 12:12:44 +0530

    Update dataset_dota.py

--- dataset_dota.py
+++ dataset_dota.py
@@ -45,7 +45,7 @@ class Dataset(Dataset):
         self.temporal_ref = 1
         self.dilation_factor = 1
         self.topk = 10
-        self.frame_stats_path = dataset_path[:-12] + 'frames_stats'  # (height, width)
+        self.frame_stats_path = dataset_path[:-8] + 'frames_stats'  # (height, width)
         self.n_frames = 50
 
         # Obj label to word embeddings
@@ -325,7 +325,7 @@ class CrossValDataset(Dataset):
         self.dilation_factor = 1
         self.toas_files_path = toas_files_path
         self.topk = 10
-        self.frame_stats_path = dataset_path[:-8] + 'frames_stats'  # (height, width)
+        self.frame_stats_path = dataset_path[:-12] + 'frames_stats'  # (height, width)
         self.n_frames = n_frames
         self.ego_dist = ego_dist
 
