7df258c2782d3c01dc4c5937611acae7c34caaab
Vipooshan Vipulananthan <77396714+Vipooshan-1998@users.noreply.github.com>
2025-10-29 14:56:15 +0530

    Update cross_validate_dota.py

--- cross_validate_dota.py
+++ cross_validate_dota.py
@@ -177,7 +177,7 @@ def test_model(epoch, model, test_dataloader, fold):
 def train(train_dataloader, test_dataloader, fold):
 
     # Define network         # Trans_LSTM
-    model = LSTM_Only(input_dim=opt.input_dim, embedding_dim=opt.embedding_dim,
+    model = LSTM_Trans(input_dim=opt.input_dim, embedding_dim=opt.embedding_dim,
                                   img_feat_dim=opt.img_feat_dim, num_classes=opt.num_classes).to(device)
     print(model)
 
