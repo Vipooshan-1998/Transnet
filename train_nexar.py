1bf8a09a842a411b470e33e3023c9106520f80bb
Vipooshan Vipulananthan <77396714+Vipooshan-1998@users.noreply.github.com>
2025-11-09 21:38:04 +0530

    Update train_nexar.py

--- train_nexar.py
+++ train_nexar.py
@@ -199,7 +199,7 @@ def main():
     test_dataloader = DataLoader(test_dataset, batch_size=opt.test_video_batch_size, shuffle=False, num_workers=8)
 
     # Define network
-    model =  Trans_LSTM_Sans_Img_LSTM(input_dim=opt.input_dim, embedding_dim=opt.embedding_dim,
+    model =  Trans_LSTM(input_dim=opt.input_dim, embedding_dim=opt.embedding_dim,
                                   img_feat_dim=opt.img_feat_dim, num_classes=opt.num_classes).to(device)
     print(model)
 
