2e72a028ac0255817ccaf54ec08cc4d5f8daf4f7
Vipooshan Vipulananthan <77396714+Vipooshan-1998@users.noreply.github.com>
2026-06-14 20:39:40 +0530

    Change model to Trans_LSTM_Sans_Object

--- cross_validate_dota.py
+++ cross_validate_dota.py
@@ -180,7 +180,7 @@ def test_model(epoch, model, test_dataloader, fold):
 def train(train_dataloader, test_dataloader, fold):
 
     # Define network         # Trans_LSTM      Trans_LSTM_Sans_Img_LSTM
-    model = Trans_LSTM(input_dim=opt.input_dim, embedding_dim=opt.embedding_dim,
+    model = Trans_LSTM_Sans_Object(input_dim=opt.input_dim, embedding_dim=opt.embedding_dim,
                                   img_feat_dim=opt.img_feat_dim, num_classes=opt.num_classes).to(device)
     print(model)
 
